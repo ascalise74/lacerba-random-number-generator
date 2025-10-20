2983396330439ff85e0660deb4b4f50c6cefb84f
ascalise <calabrone74@gmail.com>
2025-10-20 18:32:15 +0200

    Rilascio last RNG

--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
-console.log("Hello World");
+import { off } from "process";
+
+function RNG(min: number, max: number) {
+    const Nrandom = Math.random();
+    return Math.trunc((max - min) * Nrandom + min);
+}
+
+function RNGDec(min: number, max: number, precision: number) {
+    const Nrandom = Math.random();
+    let result: string;
+    result = ((max - min) * Nrandom + min).toFixed(precision);
+    return result;
+}
+
+
+function RNGSequence(min: number, max: number, len: number): number[] {
+    let myarray: number[] = [];
+    while (myarray.length < len) {
+        let numero = RNG(min, max);
+        if (myarray.includes(numero)) {
+            continue;
+        }
+        myarray.push(numero);
+    }
+    return myarray;
+}
+
+let ArrayRNG: number[] = [];
+ArrayRNG = RNGSequence(0, 100, 10);
+
+console.log(ArrayRNG);
+
+
+
